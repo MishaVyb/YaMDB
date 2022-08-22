62b0781f00284e20b94da676721ed001fb9f318d
Alexey Radchenko <alexeyra@yandex.ru>
2022-08-22 21:15:33 +0300

    - initial commit:   создана модель БД

---
+++ api_yamdb/api_yamdb/models.py
@@ -0,0 +1,108 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    role = models.CharField()
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+
+
+class Category(models.Model):
+    """Модель таблицы Category."""
+
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    """Модель таблицы Genre."""
+
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    """Модель таблицы Title."""
+
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    category = models.ForeignKey(Category,
+                                 related_name='titles',
+                                 on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre,
+                                    through='Genre_Title')
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre_Title(models.Model):
+    """Модель таблицы Genre-Title."""
+
+    title = models.ForeignKey(Title,
+                              on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre,
+                              on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    """Модель таблицы Review."""
+
+    title = models.ForeignKey(Title,
+                              related_name='reviews',
+                              on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User,
+                               related_name='reviews',
+                               on_delete=models.DO_NOTHING)
+    score = models.IntegerField()
+    pub_date = models.DateTimeField()
+
+    class Meta:
+        verbose_name = 'Обзор'
+        verbose_name_plural = 'Обзоры'
+
+    def __str__(self):
+        return self.text[:40]
+
+
+class Comments(models.Model):
+    """Модель таблицы Comments."""
+
+    review = models.ForeignKey(Review,
+                               related_name='comments',
+                               on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User,
+                               related_name='comments',
+                               on_delete=models.DO_NOTHING)
+    pub_date = models.DateTimeField()
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text[:40]
