e9072978d16ed5ae0123bca3a9d79428f8f4f804
AlxRadchenko <alexeyra@yandex.ru>
2022-08-23 13:32:42 +0300

    - первый коммит ветки   - созданы файлы в приложении api для обработки эндпойнтов reviews и comments

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+from rest_framework.relations import SlugRelatedField
+
+from review.models import Review, Comment
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from reviews.models import Review, Comment
+from rest_framework import viewsets, permissions, mixins, filters
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from .permissions import AuthorAllOthersReadOnly
+from .serializers import (CommentSerializer, ReviewSerializer)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    permission_classes = (AuthorAllOthersReadOnly,)
+
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    # pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = (AuthorAllOthersReadOnly,)
+
+    queryset = Comment.objects.all()
+    serializer_class = ReviewSerializer
+    # pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
