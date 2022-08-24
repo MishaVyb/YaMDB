6fc22a556b1fe1f9bd357c53bad9d5edbd58fbe7
AlxRadchenko <alexeyra@yandex.ru>
2022-08-24 09:47:02 +0300

    - косметические правки

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -5,7 +5,8 @@ from reviews.choices import ROLE_CHOICES
 
 class User(AbstractUser):
 
-    role = models.CharField(choices=ROLE_CHOICES, max_length=10)
+    role = models.CharField(choices=ROLE_CHOICES, max_length=10,
+                            default=ROLE_CHOICES[0])
     bio = models.TextField(
         'Биография',
         blank=True,
